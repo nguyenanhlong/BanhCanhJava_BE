5cff86a78ba26955d39c21f6e2c2d17060d47758
nguyenanhlong <vu4939351@gmail.com>
2026-06-23 18:22:27 +0700

    ci: chuyển sang luồng kiểm tra build tinh gọn không dùng ssh

--- src/main/java/com/example/banhcanh/controller/AuthController.java
+++ src/main/java/com/example/banhcanh/controller/AuthController.java
@@ -40,6 +40,7 @@ public class AuthController {
                     response.put("username", user.getUsername());
                     response.put("email", user.getEmail());
                     response.put("role", user.getRole());
+                    response.put("message", "Đăng nhập thành công!");
                     return ResponseEntity.ok(response);
                 }
                 return ResponseEntity.badRequest().body("Mật khẩu không chính xác!");
