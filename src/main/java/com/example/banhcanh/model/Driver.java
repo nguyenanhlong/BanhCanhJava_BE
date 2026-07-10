1fee4567b613193db7d812439f448ac656addc6f
nguyenanhlong <vu4939351@gmail.com>
2026-07-11 04:00:16 +0700

    Supabase

--- src/main/java/com/example/banhcanh/model/Driver.java
+++ src/main/java/com/example/banhcanh/model/Driver.java
@@ -1,8 +1,15 @@
 package com.example.banhcanh.model;
 
-import lombok.Data;
-import jakarta.persistence.*;
 import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import lombok.Data;
 
 @Data
 @Entity
