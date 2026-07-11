32d381ec18109dcf055488986d7429d8d0e61ee5
Tuan <tuan@dev.com>
2026-07-11 19:04:24 +0700

    Replace code with origin/main at commit 5376275

--- src/main/java/com/example/banhcanh/model/Driver.java
+++ src/main/java/com/example/banhcanh/model/Driver.java
@@ -1,8 +1,15 @@
 package com.example.banhcanh.model;
 
-import lombok.Data;
-import jakarta.persistence.*;
 import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import lombok.Data;
 
 @Data
 @Entity
@@ -31,7 +38,7 @@ public class Driver {
     private String vehicleColor;
 
     @Lob
-    @Column(name = "avatar_url", columnDefinition = "LONGTEXT")
+    @Column(name = "avatar_url", columnDefinition = "TEXT")
     private String avatarUrl;
 
     @Column(length = 100)
